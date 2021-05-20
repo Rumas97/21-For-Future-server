570d63028f1cba8adc9f10ff56e232e0540e6b75
Rutul Maskar <rutulmaskar23@gmail.com>
2021-05-20 11:56:46 +0200

    google routes updated and done

--- routes/google.routes.js
+++ routes/google.routes.js
@@ -8,14 +8,24 @@ router.post("/google/info", (req, res, next) => {
   const {email,googleId} = req.body
   // the name itself will include the last name
   try {
-    // Create the user in the DB
-    UserModel.create({email, googleId})
-      .then((response) => {
-        // Save the loggedInInfo in the session
-        // We'll stick to using sessions just to not over complicate the students with tokens and cookies
-        req.session.loggedInUser = response
-        res.status(200).json({data: response})
-      })
+    UserModel.find({email})
+    .then((result)=>{
+      if(result.length == 0){
+        UserModel.create({email, googleId})
+        .then((response) => {
+          // Save the loggedInInfo in the session
+          req.session.loggedInUser = response
+          res.status(200).json({data: response})
+        })
+      }
+      else{
+        req.session.loggedInUser = result[0]
+        res.status(200).json({data: result[0]})
+      }
+    })
+    .catch(()=>{
+      console.log("email not found")
+    })
   }
   catch(error) {
     res.status(500).json({error: `${error}`})
