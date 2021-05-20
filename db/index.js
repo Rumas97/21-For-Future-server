b04bc234ffeb875207a94653452c5ec0b161e1f6
Rutul Maskar <rutulmaskar23@gmail.com>
2021-05-21 00:18:58 +0200

    deploy version 1

--- db/index.js
+++ db/index.js
@@ -5,7 +5,7 @@ const mongoose = require("mongoose")
 // ℹ️ Sets the MongoDB URI for our app to have access to it.
 // If no env has been set, we dynamically set it to whatever the folder name was upon the creation of the app
 
-const MONGO_URI = process.env.MONGODB_URI || "mongodb+srv://rutul:Ironhack2021@cluster0.krjgm.mongodb.net/final-project?retryWrites=true&w=majority"
+const MONGO_URI = process.env.MONGODB_URI || "mongodb://localhost/21-for-future"
 // "mongodb://localhost/21-for-future"
 //mongodb+srv://rutul:Ironhack2021@cluster0.krjgm.mongodb.net/final-project?retryWrites=true&w=majority
 
